1be678530282f6d417a7b210f9e85c112a5df609
buluttuncel <tuncelbu@gmail.com>
2022-08-16 16:23:51 +0100

    all done but without style

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
@@ -17,9 +19,25 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
   get 'tasks', to: 'tasks#index', as: :tasks
-  get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/new', to: 'tasks#new', as: :new_task
   post 'tasks', to: 'tasks#create'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  get 'tasks/:id', to: 'tasks#show', as: :task
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
